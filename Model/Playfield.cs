a83a67d271cd3d0dde086de1902dbf06399aef31
Renato Feroce <rferoce@avans.nl>
2017-09-26 20:45:17 +0200

    LoadPlayField

--- Model/Playfield.cs
+++ Model/Playfield.cs
@@ -22,38 +22,84 @@ namespace SokoBan
             // convert level to objects of right classes
             string[] lines = System.IO.File.ReadAllLines((@"\\Mac\Home\Downloads\Doolhof\doolhof" + level + ".txt"));
 
+            bool isFirstCharAndLine = true;
+            bool isFirstLine = true;
+            bool isFirstChar = true;
+
             foreach (string line in lines)
             {
-                Console.WriteLine(lines[0][0]);
                 foreach (char c in line)
                 {
+                    Tile newTile;
                     switch (c)
                     {
                         case '#':
                             // muur
-                            new Wall();
+                            newTile = new Wall();
                             break;
                         case '.':
                             // vloer
-                            new Floor();
+                            newTile = new Floor();
                             break;
                         case 'o':
                             // krat
-                            new Box();
+                            newTile = new Box();
                             break;
                         case 'x':
                             // bestemming
-                            new Floor();
+                            newTile = new Floor();
                             break;
                         case '@':
                             // truck
-                            new Forklifttruck();
+                            newTile = new Forklifttruck();
                             break;
                         default:
                             // Empty aanmaken
+                            newTile = new Empty();
                             break;
                     }
+                    if (isFirstLine)
+                    {
+                        if (isFirstCharAndLine)
+                        {
+                            map.First = newTile;
+                            map.Current = map.First;
+                            isFirstCharAndLine = false;
+                        }
+                        else
+                        {
+                            map.Current.RightTile = newTile;
+                            newTile.LeftTile = map.Current;
+                            map.Current = map.Current.RightTile;
+                        }
+                    }
+                    else
+                    {
+                        if (isFirstChar)
+                        {
+                            Tile currentTile = map.First;
+                            while (currentTile.LowerTile != null)
+                            {
+                                currentTile = currentTile.LowerTile;
+                            }
+
+                            currentTile.LowerTile = newTile;
+                            newTile.UpperTile = currentTile;
+                            map.Current = currentTile.LowerTile;
+                            isFirstChar = false;
+                        }
+                        else
+                        {
+                            map.Current.RightTile = newTile;
+                            newTile.LeftTile = map.Current;
+                            newTile.UpperTile = map.Current.UpperTile.RightTile;
+                            map.Current.UpperTile.RightTile.LowerTile = newTile;
+                            map.Current = map.Current.RightTile;
+                        }
+                    }
                 }
+
+                isFirstLine = false;
             }
         }
     }
